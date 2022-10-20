d8fbd5c66ee6db85327d3aa5ac7528b5870deece
b0esium <b0esium@protonmail.com>
2022-10-20 14:22:13 +0200

    Complete game with no UI

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const options = ["Rock", "Paper", "Scissors"];
   let random = Math.floor(Math.random() * options.length);
@@ -15,16 +18,33 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection == "Paper" && computerSelection == "Rock") ||
     (playerSelection == "Scissors" && computerSelection == "Paper")
   ) {
+    playerScore++;
     return "You win! " + playerSelection + " beats " + computerSelection;
   } else if (
     (playerSelection == "Rock" && computerSelection == "Paper") ||
     (playerSelection == "Paper" && computerSelection == "Scissors") ||
     (playerSelection == "Scissors" && computerSelection == "Rock")
   ) {
+    computerScore++;
     return "You lose! " + computerSelection + " beats " + playerSelection;
   } else {
     return "It's a draw!";
   }
 }
 
-playRound("scissors");
+function game() {
+  // reset scores
+  playerScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    playRound(prompt("Your move?"));
+  }
+  if (playerScore > computerScore) {
+    return "You win! " + playerScore + "-" + computerScore;
+  } else if (playerScore < computerScore) {
+    return "You lose! " + playerScore + "-" + computerScore;
+  } else {
+    return "It's a draw!";
+  }
+}
