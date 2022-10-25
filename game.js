ea96325d38ff9c1ce7410fb37805bf3e93e32004
b0esium <b0esium@protonmail.com>
2022-10-25 14:58:00 +0200

    Refactor to modularize and clarify

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
+// initialize options and scores
 const options = ["rock", "paper", "scissors"];
+const maxScore = 5;
 let playerScore = 0;
 let computerScore = 0;
-const maxScore = 1;
 
 // get DOM elements
 const container = document.getElementById("container");
@@ -9,65 +10,47 @@ const resultDiv = document.getElementById("results");
 const scoreDiv = document.getElementById("score");
 const winnerDiv = document.getElementById("winner");
 
-// TODO: refactor click callback into a playGame()
+// create game buttons
 for (option of options) {
   const optionBtn = document.getElementById(option);
   optionBtn.addEventListener("click", () => {
-    // don't play if game is finished
-    if (playerScore == maxScore || computerScore == maxScore) {
-      return;
-    }
-
-    // otherwise, play
-    const result = playRound(option);
-
-    // display result in UI
-    resultDiv.innerText = result;
-
-    // display score in UI
-    scoreDiv.innerText =
-      "Your score: " + playerScore + " - Computer score: " + computerScore;
-
-    // announce winner
-    if (playerScore == maxScore || computerScore == maxScore) {
-      // make a "New game" button appear
-      const newGameBtn = document.createElement("button");
-      newGameBtn.innerHTML = "New game";
-      newGameBtn.setAttribute("id", "newGameBtn");
-      newGameBtn.addEventListener("click", () => {
-        resetGame();
-      });
-      container.appendChild(newGameBtn);
-      // declare winner
-      if (playerScore == maxScore) {
-        winnerDiv.innerText = "You win the game!";
-      } else if (computerScore == maxScore) {
-        winnerDiv.innerText = "You lose the game!";
-      }
-    }
+    playGame(option);
   });
 }
 
-function resetGame() {
-  // reset scores
-  playerScore = 0;
-  computerScore = 0;
-  // reset UI
-  resultDiv.innerText = "";
-  scoreDiv.innerText = "";
-  winnerDiv.innerText = "";
-  const newGameBtn = document.getElementById("newGameBtn");
-  container.removeChild(newGameBtn);
-}
+function playGame(move) {
+  // don't play if game is finished
+  if (playerScore == maxScore || computerScore == maxScore) {
+    return;
+  }
 
-function getComputerChoice() {
-  let random = Math.floor(Math.random() * options.length);
-  return options[random];
-}
+  // otherwise, play
+  const result = playRound(move);
 
-// normalize to first-letter capital
-function capitalize(word) {
-  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  // display result in UI
+  resultDiv.innerText = result;
+
+  // display score in UI
+  scoreDiv.innerText =
+    "Your score: " + playerScore + " - Computer score: " + computerScore;
+
+  // announce winner
+  if (playerScore == maxScore || computerScore == maxScore) {
+    // make a "New game" button appear
+    const newGameBtn = document.createElement("button");
+    newGameBtn.innerHTML = "New game";
+    newGameBtn.setAttribute("id", "newGameBtn");
+    newGameBtn.addEventListener("click", () => {
+      resetGame();
+    });
+    container.appendChild(newGameBtn);
+    // declare winner
+    if (playerScore == maxScore) {
+      winnerDiv.innerText = "You win the game!";
+    } else if (computerScore == maxScore) {
+      winnerDiv.innerText = "You lose the game!";
+    }
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -91,4 +74,27 @@ function playRound(playerSelection, computerSelection) {
   } else {
     return "It's a draw!";
   }
+}
+
+// make computer play
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * options.length);
+  return options[random];
+}
+
+// helper to normalize to first-letter capital
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
+
+function resetGame() {
+  // reset scores
+  playerScore = 0;
+  computerScore = 0;
+  // reset UI
+  resultDiv.innerText = "";
+  scoreDiv.innerText = "";
+  winnerDiv.innerText = "";
+  const newGameBtn = document.getElementById("newGameBtn");
+  container.removeChild(newGameBtn);
 }
